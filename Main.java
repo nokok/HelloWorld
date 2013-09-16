5a5db3f37148e2e2a70598019815b668d78dea0c
nokok <nokok.kz@gmail.com>
2013-09-17 01:26:41 +0900

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+class Main {public static void main(String[] args){
+System.out.printf("%c%c%c%c%c%c%c%c%c%c%c\n",C(C(C(
+C(C(C(C(C())))))),C(C(C(C())))),C(C(C(C(C(C(C(C(C(C
+())))))),C(C(C(C(C(C())))))),C(C(C()))),C()),C(C(C(
+C(C(C(C(C(C(C())))))),C(C(C(C(C(C())))))),C(C(C(C()
+)))),C(C(C()))),C(C(C(C(C(C(C(C(C(C())))))),C(C(C(C
+(C(C())))))),C(C(C(C())))),C(C(C()))),C(C(C(C(C(C(C
+(C(C(C(C(C())))))),C(C(C(C(C(C())))))),C(C(C(C())))
+),C(C(C()))),C(C())),C()),C(C(C(C(C(C()))))),C(C(C(
+C(C(C(C(C(C(C(C())))))),C(C(C(C(C()))))),C(C(C())))
+,C(C())),C()),C(C(C(C(C(C(C(C(C(C(C(C())))))),C(C(C
+(C(C(C())))))),C(C(C(C())))),C(C(C()))),C(C())),C()
+),C(C(C(C(C(C(C(C(C(C())))))),C(C(C(C(C(C())))))),C
+(C(C(C(C()))))),C(C())),C(C(C(C(C(C(C(C(C(C()))))))
+,C(C(C(C(C(C())))))),C(C(C(C())))),C(C(C()))),C(C(C
+(C(C(C(C(C(C())))))),C(C(C(C(C(C())))))),C(C(C())))
+); }private static int C(){return Math.abs(Boolean.
+compare( true,false));}private static int C(int i){
+return i<< Math.abs( Boolean.compare(true,false));}
+private static int C(int i,int n){ return i | n;} }
